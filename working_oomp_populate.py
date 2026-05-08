37a3352308dee7ff3c8c78b15f049b090010a43e
Oom Lout <aaron@oomlout.com>
2026-05-08 21:25:07 +0100

    Update working_oomp_populate.py

--- working_oomp_populate.py
+++ working_oomp_populate.py
@@ -49,29 +49,33 @@ def main(**kwargs):
     if True:        
         current_sheet = "a4"
         current_weight = "80_gsm"
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "blue_light_colour",          "taxonomy_13": "sky_blue",    "taxonomy_14": "papago","taxonomy_15": "21514"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "blue_light_colour",          "taxonomy_6": "sky_blue",    "taxonomy_14": "papago","taxonomy_15": "21514"})
         options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "green_flourescent_colour",                          "taxonomy_14": "papago","taxonomy_15": "21403"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "grey_dark_colour",    "taxonomy_13": "steel_grey",    "taxonomy_14": "papago","taxonomy_15": "21220"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "orange_colour",          "taxonomy_13": "deep_tangerine",    "taxonomy_14": "papago","taxonomy_15": "21205"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "pink_pale_colour",          "taxonomy_13": "peach",    "taxonomy_14": "papago","taxonomy_15": "21401"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "pink_pale_colour",       "taxonomy_13": "wild_rose",    "taxonomy_14": "papago","taxonomy_15": "21227"})        
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "purple_light_colour",    "taxonomy_13": "lilac",               "taxonomy_14": "papago","taxonomy_15": "21202"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_light_colour",    "taxonomy_13": "canary",    "taxonomy_14": "papago","taxonomy_15": "21207"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_light_colour",    "taxonomy_13": "daffodil",    "taxonomy_14": "papago","taxonomy_15": "21228"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "grey_dark_colour",    "taxonomy_6": "steel_grey",    "taxonomy_14": "papago","taxonomy_15": "21220"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "orange_colour",          "taxonomy_6": "deep_tangerine",    "taxonomy_14": "papago","taxonomy_15": "21205"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "pink_pale_colour",          "taxonomy_6": "peach",    "taxonomy_14": "papago","taxonomy_15": "21401"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "pink_pale_colour",       "taxonomy_6": "wild_rose",    "taxonomy_14": "papago","taxonomy_15": "21227"})        
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "purple_light_colour",    "taxonomy_6": "lilac",               "taxonomy_14": "papago","taxonomy_15": "21202"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_light_colour",    "taxonomy_6": "canary",    "taxonomy_14": "papago","taxonomy_15": "21207"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_light_colour",    "taxonomy_6": "daffodil",    "taxonomy_14": "papago","taxonomy_15": "21228"})
 
         current_weight = "100_gsm"
         options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "white_colour"})
-        
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "white_colour", "taxonomy_6": "glossy_finish", "taxonomy_7": "textured", "taxonomy_8" : "watermarked"})
 
         current_weight = "160_gsm"
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "blue_colour",    "taxonomy_13": "kingfisher_blue",    "taxonomy_14": "colorit","taxonomy_15": "387_969"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "green_colour",    "taxonomy_13": "emerald_green",    "taxonomy_14": "colorit","taxonomy_15": "387_960"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "green_colour",    "taxonomy_13": "forest_green",    "taxonomy_14": "image_coloraction","upc": "03597320018206"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "orange_colour",    "taxonomy_13": "tangerine",    "taxonomy_14": "papago","taxonomy_15": "21264"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "purple_light_colour",    "taxonomy_13": "lilac",               "taxonomy_14": "papago","taxonomy_15": "21262"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_colour",    "taxonomy_13": "golden_yellow",    "taxonomy_14": "colorit","taxonomy_15": "387_942"})
-        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_colour",    "taxonomy_13": "intensive_yellow",    "taxonomy_14": "papago","taxonomy_15": "21275"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "blue_colour",    "taxonomy_6": "kingfisher_blue",    "taxonomy_14": "colorit","taxonomy_15": "387_969"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "green_colour",    "taxonomy_6": "emerald_green",    "taxonomy_14": "colorit","taxonomy_15": "387_960"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "green_colour",    "taxonomy_6": "forest_green",    "taxonomy_14": "image_coloraction","upc": "03597320018206"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "orange_colour",    "taxonomy_6": "tangerine",    "taxonomy_14": "papago","taxonomy_15": "21264"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "purple_light_colour",    "taxonomy_6": "lilac",               "taxonomy_14": "papago","taxonomy_15": "21262"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_colour",    "taxonomy_6": "golden_yellow",    "taxonomy_14": "colorit","taxonomy_15": "387_942"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "yellow_colour",    "taxonomy_6": "intensive_yellow",    "taxonomy_14": "papago","taxonomy_15": "21275"})
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "white_colour", "taxonomy_6": "glossy_finish", "taxonomy_7": "single_sided_printing"})
 
+        #a3
+        current_weight = "80_gsm"
+        options.append({"taxonomy_3": current_sheet, "taxonomy_4": current_weight,       "taxonomy_5": "white_colour"})
 
         current_sheet = "postcard_152_4_mm_width_101_6_mm_height"
         weights = ["230_gsm"]
